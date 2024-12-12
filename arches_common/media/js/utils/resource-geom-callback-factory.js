c455b4fbce816d9bf1fec790bff02caae56ebb9d
brett <brett@qedsystems.ca>
2024-12-12 13:03:17 -0800

    Add commented out example of callback logic

--- arches_common/media/js/utils/resource-geom-callback-factory.js
+++ arches_common/media/js/utils/resource-geom-callback-factory.js
@@ -2,6 +2,10 @@ define([ ], function($, _, arches, ko, BaseFilter, MapComponentViewModel, binFea
     let resourceGeomCallbackFactory = {
         // Return a function that can extract the geometry from the resource model
         getCallbackForFeature: function(feature) {
+            // if (feature.sourceLayer === "2336968c-1035-11ec-a3aa-5254008afee6") // BC Fossil Site
+            // {
+            //     return this.fossilSiteCallback;
+            // } else ...
             return function(resource) { return {}; }
         }
     };
