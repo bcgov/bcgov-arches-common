63f941237f742888c5d875ac7cde7ad2aac02636
brett <brett@qedsystems.ca>
2024-12-12 16:02:47 -0800

    Rename package for clarity

--- arches_common/hosts.py
+++
@@ -1,9 +0,0 @@
-import re
-from django_hosts import patterns, host
-
-host_patterns = patterns(
-    "",
-    host(
-        re.sub(r"_", r"-", r"arches_common"), "arches_common.urls", name="arches_common"
-    ),
-)
