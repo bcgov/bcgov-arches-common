953ee238916c007c879790253416eb4001dbc697
brett <brett@qedsystems.ca>
2024-12-12 16:39:53 -0800

    Nit

--- bcgov_arches_common/hosts.py
+++ bcgov_arches_common/hosts.py
@@ -4,6 +4,8 @@ from django_hosts import patterns, host
 host_patterns = patterns(
     "",
     host(
-        re.sub(r"_", r"-", r"bcgov_arches_common"), "bcgov_arches_common.urls", name="bcgov_arches_common"
+        re.sub(r"_", r"-", r"bcgov_arches_common"),
+        "bcgov_arches_common.urls",
+        name="bcgov_arches_common",
     ),
 )
