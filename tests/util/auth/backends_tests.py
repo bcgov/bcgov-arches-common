169e3b191ea4903d2a41e1ee2fd64f17241141fb
brett <brett@qedsystems.ca>
2024-12-13 20:31:38 -0800

    Nit

--- tests/util/auth/backends_tests.py
+++ tests/util/auth/backends_tests.py
@@ -1,5 +1,3 @@
-from unittest.mock import Mock
-
 from bcgov_arches_common.util.auth.backends import BCGovRemoteUserBackend
 from django.test import TestCase
 
@@ -7,8 +5,12 @@ from django.test import TestCase
 class TestCleanUsername(TestCase):
     def test_clean_username(self):
         remote_user_backend = BCGovRemoteUserBackend()
-        self.assertEqual(remote_user_backend.clean_username("SOME_USERNAME"), "some_username")
-        self.assertEqual(remote_user_backend.clean_username("idir\SOME_USERNAME"), "some_username")
-        self.assertEqual(remote_user_backend.clean_username("IDIR\SOME_USERNAME"), "some_username")
-
-
+        self.assertEqual(
+            remote_user_backend.clean_username("SOME_USERNAME"), "some_username"
+        )
+        self.assertEqual(
+            remote_user_backend.clean_username("idir\SOME_USERNAME"), "some_username"
+        )
+        self.assertEqual(
+            remote_user_backend.clean_username("IDIR\SOME_USERNAME"), "some_username"
+        )
