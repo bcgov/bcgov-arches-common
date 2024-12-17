862e975aec6f53f47f65c577fcc8334540c943f5
brett <brett@qedsystems.ca>
2024-12-17 15:27:58 -0800

    Fix pyproject dependencies

--- bcgov_arches_common/apps.py
+++ bcgov_arches_common/apps.py
@@ -6,7 +6,7 @@ from arches.settings_utils import generate_frontend_configuration
 
 class ArchesCommonConfig(AppConfig):
     name = "bcgov_arches_common"
-    is_arches_application = True
+    is_arches_application = False
 
     def ready(self):
         if settings.APP_NAME.lower() == self.name:
