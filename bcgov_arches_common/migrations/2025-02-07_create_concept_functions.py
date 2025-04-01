2581314c9a6fcb82459a6c040cd1ed6209ca3e4b
brett <brett@qedsystems.ca>
2025-04-01 09:03:14 -0700

    Apply black format to migrations

--- bcgov_arches_common/migrations/2025-02-07_create_concept_functions.py
+++ bcgov_arches_common/migrations/2025-02-07_create_concept_functions.py
@@ -3,18 +3,29 @@ import os
 from .util.migration_util import format_files_into_sql
 from django.conf import settings
 
+
 class Migration(migrations.Migration):
-    dependencies = [("models","0001_initial")]
+    dependencies = [("models", "0001_initial")]
 
     username = settings.DATABASES["default"]["USER"]
 
     sql_dir = os.path.join(os.path.dirname(__file__), "sql", "2025")
     files = [
-        format_files_into_sql("2025-02-06_get_concept_uuid.sql", sql_dir).format(app_owner= username),
-        format_files_into_sql("2025-02-06_import_vocabulary_item.sql", sql_dir).format(app_owner= username),
-        format_files_into_sql("2025-02-07_delete_flat_collection.sql", sql_dir).format(app_owner= username),
-        format_files_into_sql("2025-02-07_import_vocabulary_with_collection.sql", sql_dir).format(app_owner= username),
-        format_files_into_sql("2025-02-07_import_vocabulary_with_values.sql", sql_dir).format(app_owner= username),
+        format_files_into_sql("2025-02-06_get_concept_uuid.sql", sql_dir).format(
+            app_owner=username
+        ),
+        format_files_into_sql("2025-02-06_import_vocabulary_item.sql", sql_dir).format(
+            app_owner=username
+        ),
+        format_files_into_sql("2025-02-07_delete_flat_collection.sql", sql_dir).format(
+            app_owner=username
+        ),
+        format_files_into_sql(
+            "2025-02-07_import_vocabulary_with_collection.sql", sql_dir
+        ).format(app_owner=username),
+        format_files_into_sql(
+            "2025-02-07_import_vocabulary_with_values.sql", sql_dir
+        ).format(app_owner=username),
     ]
 
     drop_statements = [
