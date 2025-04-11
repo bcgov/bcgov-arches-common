541f1df1ccdf8b456324ebaccb37ed43592a236d
brett <brett@qedsystems.ca>
2025-04-11 13:04:48 -0700

    1264 - Functioning radio button and select components applied to step 9. Multi-object widget still showing UUIDs dev - Fix route names parameter in PageHeader and SideNav

---
+++ bcgov_arches_common/src/bcgov_arches_common/components/ConceptSelect/ConceptRadioButtons.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { onMounted, ref, computed } from 'vue';
+import { getConceptsForNode } from '@/bcgov_arches_common/api.ts';
+import RadioButton from 'primevue/radiobutton';
+import RadioButtonGroup from 'primevue/radiobuttongroup';
+
+
+const model = defineModel();
+const props = defineProps({
+    graphSlug: { type: String, required: true },
+    nodeAlias: { type: String, required: true },
+    id: { type: String, required: true },
+    placeholder: { type: String, default: 'Select an option' },
+    groupDirection: { type: String, default: 'column' },
+});
+
+const emit = defineEmits(['valueUpdated']);
+
+const options = ref([]);
+
+onMounted(() => {
+    getConceptsForNode(props.graphSlug, props.nodeAlias, options);
+});
+
+const valueUpdated = function (event: Event) {
+    emit('valueUpdated', event.target.value, event.target);
+};
+
+const flexDirection = computed(() => {
+    return props.groupDirection === 'column' ? 'flex-col' : 'flex-row gap-2';
+});
+</script>
+
+<template>
+    <RadioButtonGroup
+        v-model="model"
+        name="id"
+        class="flex flex-wrap"
+        :class="flexDirection"
+    >
+        <div
+            v-for="option in options"
+            :key="option.id"
+            class="flex items-center gap-2"
+        >
+            <RadioButton
+                v-model="model"
+                :input-id="option.id"
+                name="dynamic"
+                :value="option.id"
+                variant="filled"
+                size="small"
+                @change="valueUpdated"
+            />
+            <label :for="option.id">{{ option.text }}</label>
+        </div>
+    </RadioButtonGroup>
+</template>
+
+<style scoped></style>
+
+<style></style>
