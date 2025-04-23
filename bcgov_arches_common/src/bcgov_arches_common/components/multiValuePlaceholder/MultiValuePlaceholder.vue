e756d145cd224cf50e963afcd9bf4ac2d4a3993e
brett <brett@qedsystems.ca>
2025-04-23 11:37:43 -0700

    Fix prettier formatting

--- bcgov_arches_common/src/bcgov_arches_common/components/multiValuePlaceholder/MultiValuePlaceholder.vue
+++ bcgov_arches_common/src/bcgov_arches_common/components/multiValuePlaceholder/MultiValuePlaceholder.vue
@@ -3,29 +3,40 @@ import { ref, watch } from 'vue';
 
 const props = defineProps({
     label: { type: String, default: '' },
-    showDeleteButton: {type: Boolean, default: true},
-    displayValues: {type: Array<String>, required: true},
-    deleteCallback: {type: Function, required: true},
+    showDeleteButton: { type: Boolean, default: true },
+    displayValues: { type: Array<String>, required: true },
+    deleteCallback: { type: Function, required: true },
 });
 
 const displayValues = ref(props.displayValues);
 watch(
     () => props.displayValues,
-    () => { displayValues.value = props.displayValues },
-    {immediate: true}
+    () => {
+        displayValues.value = props.displayValues;
+    },
+    { immediate: true },
 );
 </script>
 
 <template>
     <div class="labelled-input flex flex-col gap-2">
-        <label>{{ props.label }} ({{displayValues.length}})</label>
-        <div v-for="(value, index) in displayValues" key="value">
+        <label>{{ props.label }} ({{ displayValues.length }})</label>
+        <div
+            v-for="(value, index) in displayValues"
+            key="value"
+        >
             <div>
-                <div class="item-index" style="display: inline-block">
+                <div
+                    class="item-index"
+                    style="display: inline-block"
+                >
                     {{ index + 1 }}
                 </div>
                 <div style="display: inline-block">
-                    <slot :value="value" :index="index"></slot>
+                    <slot
+                        :value="value"
+                        :index="index"
+                    ></slot>
                 </div>
                 <i
                     class="fa fa-trash-can item-delete-button"
@@ -39,16 +50,14 @@ watch(
 
 <style scoped>
 .item-delete-button {
-    margin-left: .5rem;
+    margin-left: 0.5rem;
     color: blue;
     cursor: pointer;
 }
 .item-index {
-    margin-right: .5rem;
+    margin-right: 0.5rem;
     min-width: 2rem;
 }
-
 </style>
 
-<style>
-</style>
+<style></style>
