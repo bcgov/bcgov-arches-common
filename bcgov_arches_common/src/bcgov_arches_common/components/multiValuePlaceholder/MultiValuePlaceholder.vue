20eac98a939ea97c9827bfdc8c7683f79354fdb4
brett <brett@qedsystems.ca>
2025-04-23 12:14:50 -0700

    Fix ESLint problems

--- bcgov_arches_common/src/bcgov_arches_common/components/multiValuePlaceholder/MultiValuePlaceholder.vue
+++ bcgov_arches_common/src/bcgov_arches_common/components/multiValuePlaceholder/MultiValuePlaceholder.vue
@@ -4,7 +4,7 @@ import { ref, watch } from 'vue';
 const props = defineProps({
     label: { type: String, default: '' },
     showDeleteButton: { type: Boolean, default: true },
-    displayValues: { type: Array<String>, required: true },
+    displayValues: { type: Array<string>, required: true },
     deleteCallback: { type: Function, required: true },
 });
 
@@ -23,7 +23,7 @@ watch(
         <label>{{ props.label }} ({{ displayValues.length }})</label>
         <div
             v-for="(value, index) in displayValues"
-            key="value"
+            :key="value"
         >
             <div>
                 <div
@@ -59,5 +59,3 @@ watch(
     min-width: 2rem;
 }
 </style>
-
-<style></style>
