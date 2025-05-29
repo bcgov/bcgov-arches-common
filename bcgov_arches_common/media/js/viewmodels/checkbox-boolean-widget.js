39c58e6efa045c831114edb457c61282597d3beb
brett <brett@qedsystems.ca>
2025-05-28 18:38:48 -0700

    214 - Update to use Arches Core 8.0.0a4, Docker Containers: add component-lab to docker containers, don't build webpack container by default, install vim by default run prettier on all javascript files

--- bcgov_arches_common/media/js/viewmodels/checkbox-boolean-widget.js
+++ bcgov_arches_common/media/js/viewmodels/checkbox-boolean-widget.js
@@ -1,40 +1,35 @@
-define([
-    'knockout',
-    'underscore',
-    'viewmodels/widget'
-], function(ko, _, WidgetViewModel) {
-    /**
-     * A viewmodel used for boolean checkbox widgets
-     *
-     * @constructor
-     * @name BooleanCheckboxWidgetViewModel
-     *
-     * @param  {string} params - a configuration object
-     */
-    var BooleanCheckboxWidgetViewModel = function(params) {
-        var self = this;
+import ko from "knockout";
+import WidgetViewModel from "viewmodels/widget";
+/**
+ * A viewmodel used for boolean checkbox widgets
+ *
+ * @constructor
+ * @name BooleanCheckboxWidgetViewModel
+ *
+ * @param  {string} params - a configuration object
+ */
+var BooleanCheckboxWidgetViewModel = function (params) {
+    var self = this;
 
-        WidgetViewModel.apply(this, [params]);
+    WidgetViewModel.apply(this, [params]);
 
-        var value = self.configForm ? self.defaultValue : self.value;
+    var value = self.configForm ? self.defaultValue : self.value;
 
-        this.toggleSelected = function() {
-            var selected = !self.isSelected();
-            console.log(`Setting selected to ${selected}`);
-            self.setSelected(selected);
-        };
-
-        this.setSelected = function(selected) {
-            if (ko.unwrap(self.disabled) === false) {
-                value(selected);
-            }
-        };
-
-        this.isSelected = function() {
-            return !!value();
-        };
-
+    this.toggleSelected = function () {
+        var selected = !self.isSelected();
+        console.log(`Setting selected to ${selected}`);
+        self.setSelected(selected);
     };
 
-    return BooleanCheckboxWidgetViewModel;
-});
+    this.setSelected = function (selected) {
+        if (ko.unwrap(self.disabled) === false) {
+            value(selected);
+        }
+    };
+
+    this.isSelected = function () {
+        return !!value();
+    };
+};
+
+export default BooleanCheckboxWidgetViewModel;
--- bcgov_arches_common/media/js/views/components/functions/unique-boolean-value.js
+++ bcgov_arches_common/media/js/views/components/functions/unique-boolean-value.js
@@ -1,47 +1,47 @@
-define(['jquery',
-    'underscore',
-    'arches',
-    'knockout',
-    'knockout-mapping',
-    'viewmodels/function',
-    'bindings/chosen',
-    'views/components/simple-switch',
-    'templates/views/components/functions/unique-boolean-value.htm'],
-function($, _, arches, ko, koMapping, FunctionViewModel, chosen, simpleSwitch, template) {
-    return ko.components.register('views/components/functions/unique-boolean-value', {
-        viewModel: function(params) {
+import $ from "jquery";
+import ko from "knockout";
+import FunctionViewModel from "viewmodels/function-view-model";
+import template from "templates/views/components/functions/unique-boolean-value.htm";
+
+export default ko.components.register(
+    "views/components/functions/unique-boolean-value",
+    {
+        viewModel: function (params) {
             FunctionViewModel.apply(this, arguments);
             var nodegroups = {};
             var sortedCards = [];
             this.triggering_nodegroups = params.config.triggering_nodegroups;
             this.loading = ko.observable(false);
 
-            this.graph.nodes.forEach(function(card){
-                if (card.datatype === 'boolean')
-                {
+            this.graph.nodes.forEach(function (card) {
+                if (card.datatype === "boolean") {
                     sortedCards.push(card);
                     nodegroups[card.nodeid] = true;
                 }
             }, this);
 
-            sortedCards.sort(function(a, b){ if ( a.name === b.name) return 0; return a.name > b.name ? 1 : -1 })
+            sortedCards.sort(function (a, b) {
+                if (a.name === b.name) return 0;
+                return a.name > b.name ? 1 : -1;
+            });
             this.cards = ko.observableArray(sortedCards);
 
-            this.unique_value = params.config.unique_value
-            this.node_id = params.config.node_id
-            this.node_id.subscribe(function(newValue) {
+            this.unique_value = params.config.unique_value;
+            this.node_id = params.config.node_id;
+            this.node_id.subscribe(function (newValue) {
                 this.node_id(newValue);
-                console.log("New value: "+newValue);
-                this.graph.nodes.forEach(function(node) {
-                    if (node.nodeid == newValue)
-                    {
+                console.log("New value: " + newValue);
+                this.graph.nodes.forEach(function (node) {
+                    if (node.nodeid == newValue) {
                         this.triggering_nodegroups([node.nodegroup_id]);
                     }
-                }, this)
+                }, this);
             }, this);
 
-            window.setTimeout(function(){$("select[data-bind^=chosen]").trigger("chosen:updated");}, 300);
+            window.setTimeout(function () {
+                $("select[data-bind^=chosen]").trigger("chosen:updated");
+            }, 300);
         },
-        template: template
-    });
-});
+        template: template,
+    },
+);
