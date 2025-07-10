c59f2827d210b9ff775136ad527d33849dd34160
Brett Ferguson <brett@qedsystems.ca>
2025-07-10 15:27:37 -0700

    494 - Fix unit test formatting (#29)
    * 494 - Fix unit test formatting
    
    * 494 - Fix unit test formatting
    
    * 494 - Fix unit test
    
    * Fixing GH testing configuration
    
    * Fixing GH unit test configuration
    
    * Fixing GH unit test configuration
    
    * Fixing GH unit test configuration
    
    * Remove unused test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -20,36 +20,21 @@ import os
 
 from bcgov_arches_common.settings import *
 
+try:
+    from bcgov_arches_common.settings_docker import *
+except ImportError:  # unable to import prior to installing requirements.txt in setup.py
+    pass
+
 PACKAGE_NAME = "bcgov_arches_common"
 
 PROJECT_TEST_ROOT = os.path.dirname(__file__)
 MEDIA_ROOT = os.path.join(PROJECT_TEST_ROOT, "fixtures", "data")
-
-BCGOV_PROXY_PREFIX = "bcgov_arches_core/"
 
 BUSINESS_DATA_FILES = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
-
-DATABASES = {
-    "default": {
-        "ATOMIC_REQUESTS": False,
-        "AUTOCOMMIT": True,
-        "CONN_MAX_AGE": 0,
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "HOST": "localhost",
-        "NAME": "bcgov_arches_common",
-        "OPTIONS": {},
-        "PASSWORD": "postgis",
-        "PORT": "5432",
-        "POSTGIS_TEMPLATE": "template_postgis",
-        "TEST": {"CHARSET": None, "COLLATION": None, "MIRROR": None, "NAME": None},
-        "TIME_ZONE": None,
-        "USER": "postgres",
-    }
-}
 
 CACHES = {
     "default": {
@@ -69,7 +54,3 @@ TEST_RUNNER = "arches.test.runner.ArchesTestRunner"
 SILENCED_SYSTEM_CHECKS.append(
     "arches.W001",  # Cache backend does not support rate-limiting
 )
-
-ELASTICSEARCH_HOSTS = [
-    {"scheme": "http", "host": "localhost", "port": ELASTICSEARCH_HTTP_PORT}
-]
