5fc1e441622227194107e76b7fc0d16ef0755126
brett <brett@qedsystems.ca>
2025-09-05 11:13:51 -0700

    Create standard DataTable component

---
+++ bcgov_arches_common/src/bcgov_arches_common/components/StandardDataTable/StandardDataTable.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import type { ColumnDefinition } from "@/bcgov_arches_common/components/StandardDataTable/types.ts";
+import type { AliasedNodeData } from "@/arches_component_lab/types.ts";
+import {
+    getNodeDisplayValue,
+    labelize,
+} from "@/bcgov_arches_common/datatypes/utils.ts";
+import { computed } from "vue";
+
+const { title, columnDefinitions, tableData, initialSortFieldIndex } =
+    defineProps<{
+        title?: string;
+        columnDefinitions: ColumnDefinition[];
+        tableData: AliasedNodeData[];
+        initialSortFieldIndex?: number;
+    }>();
+
+const isSortable = (col: ColumnDefinition) => {
+    return col.sortable ?? true;
+};
+
+const tableTitle = computed(() => {
+    return title ?? "";
+});
+
+const initialSortField = computed(() => {
+    return initialSortFieldIndex ?? 0;
+});
+
+const columnTitle = function (colDef: ColumnDefinition) {
+    return colDef.label ?? labelize(colDef.field);
+};
+</script>
+<template>
+    <dl v-if="(tableData?.length ?? 0) > 0">
+        <dt>{{ tableTitle }}</dt>
+        <dd>
+            <DataTable
+                :value="tableData"
+                data-key="tileid"
+                responsive-layout="scroll"
+                :sort-field="`aliased_data.${columnDefinitions[initialSortField].field}.display_value`"
+                :sort-order="-1">
+                <Column
+                    v-for="col in columnDefinitions"
+                    :key="col.field"
+                    :header="columnTitle(col)"
+                    :field="`aliased_data.${col.field}.display_value`"
+                    :sortable="isSortable(col)">
+                    <template #body="slotProps">
+                        {{ getNodeDisplayValue(slotProps.data, col.field) }}
+                    </template>
+                </Column>
+            </DataTable>
+        </dd>
+    </dl>
+</template>
