b6504ae4ef53b5db08978e4ef5cb8fa75432b779
Brett Ferguson <brett@qedsystems.ca>
2025-10-20 20:07:01 -0700

    Adds generic Zod validation and additional datatype to support file-list data (#58)
    Bypassing to fix bcfms CI
    * Add file-list datatype, persist files in workflow, fix project documents shape in schema
    * Add generated unit test

---
+++ bcgov_arches_common/src/bcgov_arches_common/datatypes/file-list/types.ts
@@ -0,0 +1,12 @@
+import type { FileListValue } from '@/arches_component_lab/datatypes/file-list/types.ts';
+
+// This should probably be in arches-component-lab, or the FileReference there should
+// potentially be modified
+export type FileReference = FileListValue & {
+    file?: File;
+    node_id?: string;
+    name?: string;
+    lastModified?: number;
+    size?: number;
+    type?: string;
+};
