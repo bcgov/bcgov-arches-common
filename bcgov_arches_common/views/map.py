ca94c8b7611a95334445c9e196dd94faa5842cf1
brett <brett@qedsystems.ca>
2026-01-20 21:04:55 -0800

    Make outbound proxy configurable in settings and eliminate duplicate proxy class

--- bcgov_arches_common/views/map.py
+++ bcgov_arches_common/views/map.py
@@ -1,44 +1,57 @@
-import urllib3
 from arches.app.views.map import TileserverProxyView
 from django.conf import settings
+from bcgov_arches_common.views.outbound_proxy_mixin import OutboundProxyMixin
 
 
-class BCTileserverLocalProxyView(TileserverProxyView):
-    upstream_urls = settings.BC_TILESERVER_URLS
+class BCTileserverProxyView(TileserverProxyView, OutboundProxyMixin):
     """
     Subclass of the TileserverProxyView that has multiple upstream servers.
-        - the BC_TILESERVER_URLS is a dict with source->upstream URL mapping
-        - the request URL must have the source parameter set. If not set it defaults to the parent upstream value
+        - the BC_TILESERVER_URLS is a dict with source->upstream URL mapping and outbound proxy configuration
+        - the request URL must have the source parameter set. If not set it defaults to "openmaps"
     """
 
-    def __init__(self, *args, **kwargs):
-        super(BCTileserverLocalProxyView, self).__init__(*args, **kwargs)
+    DEFAULT_SOURCE = "openmaps"
+    DEFAULT_CONFIG = {
+        "url": getattr(settings, "TILESERVER_URL", "https://openmaps.gov.bc.ca/"),
+        "use_outbound_proxy": False,
+    }
+
+    @property
+    def upstream_urls(self):
+        """
+        Get upstream URLs from settings, ensuring the default source is always available.
+        """
+        # Get configuration from settings or empty dict if not defined
+        urls = getattr(settings, "BC_TILESERVER_URLS", {})
+
+        # Ensure default source exists
+        if self.DEFAULT_SOURCE not in urls:
+            urls[self.DEFAULT_SOURCE] = self.DEFAULT_CONFIG
+
+        return urls
 
     def get_request_headers(self):
         proxy_source = self.request.GET.get("source", "")
-        # print("\tProxy source: %s" % proxy_source)
-        if proxy_source in self.upstream_urls:
-            self.upstream = self.upstream_urls[proxy_source]
 
-        return super(BCTileserverLocalProxyView, self).get_request_headers()
+        # Get all available sources
+        upstream_urls = self.upstream_urls
 
+        # Get source configuration with fallback to default source
+        source_config = (
+            upstream_urls[proxy_source]
+            if proxy_source in upstream_urls
+            else upstream_urls[self.DEFAULT_SOURCE]
+        )
 
-class BCTileserverProxyView(BCTileserverLocalProxyView):
-    """
-    Subclass of the TileserverProxyView that has multiple upstream servers.
-        - the BC_TILESERVER_URLS is a dict with source->upstream URL mapping
-        - the request URL must have the source parameter set. If not set it defaults to the parent upstream value
-    """
+        # Update upstream URL from configuration
+        self.upstream = source_config.get("url")
 
-    def __init__(self, *args, **kwargs):
-        super(BCTileserverProxyView, self).__init__(*args, **kwargs)
-        # Setup outbound proxy if it is configured
-        if (
-            hasattr(settings, "TILESERVER_OUTBOUND_PROXY")
-            and settings.TILESERVER_OUTBOUND_PROXY
-        ):
-            # print("Setting outbound proxy to %s" % settings.TILESERVER_OUTBOUND_PROXY)
-            self.http = urllib3.ProxyManager(settings.TILESERVER_OUTBOUND_PROXY)
+        # Configure the HTTP connection based on proxy setting
+        self.http = self.get_http_connection(
+            use_outbound_proxy=source_config.get("use_outbound_proxy", False)
+        )
+
+        return super(BCTileserverProxyView, self).get_request_headers()
 
     def dispatch(self, request, *args, **kwargs):
         # When hitting /bctileserver/ there is no <path:path> kwarg.
