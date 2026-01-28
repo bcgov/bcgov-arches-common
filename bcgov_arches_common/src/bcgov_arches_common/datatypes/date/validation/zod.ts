c55bff4ebc383ddccd8e6370b17eb3ec14b16a3d
brett <brett@qedsystems.ca>
2026-01-28 11:21:43 -0800

    Add year-only zod validator

--- bcgov_arches_common/src/bcgov_arches_common/datatypes/date/validation/zod.ts
+++ bcgov_arches_common/src/bcgov_arches_common/datatypes/date/validation/zod.ts
@@ -9,3 +9,18 @@ export const DateValueSchema = z.object({
 export const DateValueRequiredSchema = DateValueSchema.safeExtend({
     node_value: z.iso.date(),
 });
+
+const baseYearValidator = z
+    .string()
+    .transform((val) => (val === '' ? null : parseInt(val, 10)))
+    .pipe(z.number().int().min(1900).max(2100));
+
+export const YearValueSchema = z.object({
+    display_value: z.string().nullish(),
+    node_value: baseYearValidator.nullable(),
+    details: z.array(z.never()),
+});
+
+export const YearValueRequiredSchema = YearValueSchema.safeExtend({
+    node_value: baseYearValidator,
+});
