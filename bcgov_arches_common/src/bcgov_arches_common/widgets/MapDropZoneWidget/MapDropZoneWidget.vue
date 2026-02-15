c1ff1483582315a57d12414382daf529c4f25cb9
Brett Ferguson <brett@qedsystems.ca>
2026-02-14 19:45:40 -0800

    Initial cut of fixing map drop zone reactivity. Needs testing. (#79)

--- bcgov_arches_common/src/bcgov_arches_common/widgets/MapDropZoneWidget/MapDropZoneWidget.vue
+++ bcgov_arches_common/src/bcgov_arches_common/widgets/MapDropZoneWidget/MapDropZoneWidget.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import MapDropZoneWidgetEditor from '@/bcgov_arches_common/widgets/MapDropZoneWidget/components/MapDropZoneWidgetEditor/MapDropZoneWidgetEditor.vue';
 import SimpleMap from '@/bcgov_arches_common/widgets/SimpleMap/SimpleMap.vue';
 import type { GeoJSONFeatureCollectionCardXNodeXWidgetData } from '@/bcgov_arches_common/datatypes/geojson-feature-collection/types.ts';
@@ -8,24 +8,41 @@ import { EDIT, VIEW } from '@/arches_component_lab/widgets/constants.ts';
 import type { WidgetMode } from '@/arches_component_lab/widgets/types.ts';
 
 import type { GeoJSONFeatureCollectionValue } from '@/bcgov_arches_common/datatypes/geojson-feature-collection/types.ts';
+import { blankGeoJSONValue } from '@/bcrhp/utils.ts';
 
-const { mode, nodeAlias, graphSlug, cardXNodeXWidgetData, aliasedNodeData } =
-    defineProps<{
-        mode: WidgetMode;
-        nodeAlias: string;
-        graphSlug: string;
-        cardXNodeXWidgetData: GeoJSONFeatureCollectionCardXNodeXWidgetData;
-        aliasedNodeData: GeoJSONFeatureCollectionValue | undefined;
-    }>();
+const props = defineProps<{
+    mode: WidgetMode;
+    nodeAlias: string;
+    graphSlug: string;
+    cardXNodeXWidgetData: GeoJSONFeatureCollectionCardXNodeXWidgetData;
+    aliasedNodeData: GeoJSONFeatureCollectionValue | undefined;
+}>();
+
+const { mode, nodeAlias, graphSlug, cardXNodeXWidgetData } = props;
 
 const emit = defineEmits(['update:value']);
 
-const aliasedNodeDataForDisplay = ref(aliasedNodeData);
+const aliasedNodeDataFromFiles = ref(blankGeoJSONValue());
 
 function updateGeometries(newValue: GeoJSONFeatureCollectionValue) {
-    aliasedNodeDataForDisplay.value = newValue;
+    aliasedNodeDataFromFiles.value = newValue;
     emit('update:value', newValue);
 }
+
+const concatenatedAliasedNodeData = computed(() => {
+    return {
+        ...aliasedNodeDataFromFiles.value,
+        node_value: {
+            ...aliasedNodeDataFromFiles.value?.node_value,
+            type: 'FeatureCollection',
+            // Combine features from both sources
+            features: [
+                ...(aliasedNodeDataFromFiles.value?.node_value?.features || []),
+                ...(props.aliasedNodeData?.node_value?.features || []),
+            ],
+        },
+    };
+});
 </script>
 
 <template>
@@ -41,7 +58,7 @@ function updateGeometries(newValue: GeoJSONFeatureCollectionValue) {
                 v-if="mode === EDIT"
                 :card-x-node-x-widget-data="cardXNodeXWidgetData"
                 :node-alias="nodeAlias"
-                :aliased-node-data="aliasedNodeData"
+                :aliased-node-data="props.aliasedNodeData"
                 @update:value="updateGeometries($event)" />
         </div>
 
@@ -59,7 +76,7 @@ function updateGeometries(newValue: GeoJSONFeatureCollectionValue) {
                 :node-alias="nodeAlias"
                 :mode="VIEW"
                 :card-x-node-x-widget-data="cardXNodeXWidgetData"
-                :aliased-node-data="aliasedNodeDataForDisplay" />
+                :aliased-node-data="concatenatedAliasedNodeData" />
         </div>
     </div>
 </template>
