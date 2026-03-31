2b334cb18fe59e08675a2e8b4441f43de5beea36
Brett Ferguson <brett@qedsystems.ca>
2026-03-31 13:16:44 -0700

    Allow BCSC usernames in OAuth session control (#110)
    * Allow BCSC usernames in OAuth session control
    
    * Allow BCSC & IDIR self registration

--- bcgov_arches_common/util/auth/oauth_session_control.py
+++ bcgov_arches_common/util/auth/oauth_session_control.py
@@ -2,14 +2,44 @@ import re
 import logging
 from django.shortcuts import redirect
 from django.contrib.auth import login as system_login, logout as system_logout
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
+
+DEFAULT_GROUPS = ["Resource Exporter"]
 
 
 def _clean_username(username):
     # DLVR: IDIR = <username>@idir, TEST, PROD: IDIR = idir\\<username>
-    return None if username is None else re.sub(r"^idir\\(.*)$", r"\1@idir", username)
+    # DLVR: BCSC = bcsc/<username>, TEST, PROD: ??
+    username = (
+        None
+        if username is None
+        else re.sub(r"^(idir|bcsc)[\\/](.*)$", r"\2@\1", username)
+    )
+    print(username)
+    return username
+
+
+def _self_register(userinfo):
+    print(userinfo)
+    user = None
+    if (
+        "allowed_self_register_domains" in settings.AUTHLIB_OAUTH_CLIENTS["default"]
+        and userinfo["loginSource"]
+        in settings.AUTHLIB_OAUTH_CLIENTS["default"]["allowed_self_register_domains"]
+    ):
+        user = User(
+            username=_clean_username(userinfo["preferred_username"]),
+            first_name=userinfo["given_name"],
+            last_name=userinfo["family_name"],
+        )
+        user.set_unusable_password()
+        user.save()
+        user.groups.set(Group.objects.filter(name__in=DEFAULT_GROUPS))
+        user.save()
+    return user
 
 
 def log_user_out(request):
@@ -23,7 +53,7 @@ def log_user_in(request, token, next_url):
         username = _clean_username(token["userinfo"]["preferred_username"])
         user = User.objects.get(username=username)
     except User.DoesNotExist:
-        user = None
+        user = _self_register(token["userinfo"])
 
     if user is not None:
         user.backend = "django.contrib.auth.backends.ModelBackend"
